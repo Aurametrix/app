f6eda54a71a70f8fe59ec53cf8e5df02f7e4295c
Irene <irene@aurametrix.com>
2014-03-11 18:11:28 -0700

    preferences form

--- main.py
+++ main.py
@@ -13,10 +13,24 @@ class MainPage(webapp.RequestHandler):
         if not user: 
             navbar = ('<p>Welcome! <a href="%s">Sign in or register</a> to customize.</p>' 
                 % (users.create_login_url(self.request.path))) 
+            tz_form = ''    
         else: 
+            userprefs = models.get_userprefs()
             navbar = ('<p>Welcome, %s! You can <a href="%s">sign out</a>.</p>' 
-                % (user.email(), users.create_logout_url(self.request.path))) 
-
+                % (user.nickname(), users.create_logout_url(self.request.path)))
+            tz_form = '''
+                 <form action="/prefs" method="post">
+                     <label for="tz_offset">
+                         Timezone offset from UTC (can be negative):
+                     </label>
+                     <input name="tz_offset" id="tz_offset" type="text" 
+                            size="4" value="%d" />
+                     <input type="submit" value="Set" />
+                 </form>
+            ''' % userprefs.tz_offset
+             
+            time += datetime.timedelta(0, 0, 0, 0, 0, userprefs.tz_offset)
+  
         self.response.headers['Content-Type'] = 'text/html' 
         self.response.out.write(''' 
         <html> 
@@ -26,9 +40,10 @@ class MainPage(webapp.RequestHandler):
             <body> 
             %s 
                 <p>The time is: %s</p> 
+            %s 
             </body> 
         </html> 
-        ''' % (navbar, str(time))) 
+        ''' % (navbar, str(time), tz_form)) 
 
 application = webapp.WSGIApplication([('/', MainPage)],
                                      debug=True)
