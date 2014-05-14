566b10d1145e28e6495a56268f6df2697561b026
Irene <irene@aurametrix.com>
2014-05-13 20:28:41 -0700

    caching UserPrefs objects in memcache

--- models.py
+++ models.py
@@ -1,9 +1,17 @@
+from google.appengine.api import memcache
 from google.appengine.api import users
 from google.appengine.ext import db
 
 class UserPrefs(db.Model):
     tz_offset = db.IntegerProperty(default=0)
     user = db.UserProperty(auto_current_user_add=True)
+
+def cache_set(self): 
+    memcache.set(self.key().name(), self, namespace=self.key().kind())
+
+def put(self): 
+    self.cache_set() 
+    db.Model.put(self)
 
 def get_userprefs(user_id=None):
     if not user_id:
@@ -12,8 +20,7 @@ def get_userprefs(user_id=None):
             return None
         user_id = user.user_id()
         
-    key = db.Key.from_path('UserPrefs', user_id)
-    userprefs = db.get(key)
+    userprefs = memcache.get(user_id, namespace='UserPrefs')
     if not userprefs:
             userprefs = UserPrefs(key_name=user_id)
 
